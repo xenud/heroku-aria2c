96f6259db4809529d05346a525b8779c25a3b45f
xenud <lazzaronipaolo@outlook.it>
2020-03-14 11:51:58 +0100

    Aggiunto 2° remote e comando custom

--- on-complete.sh
+++ on-complete.sh
@@ -21,9 +21,35 @@ echo -e "$(date +"%m/%d %H:%M:%S") ${INFO} Delete .aria2 file finish"
 echo "$(($(cat numUpload)+1))" > numUpload # Plus 1
 
 if [[ $2 -eq 1 ]]; then # single file
-	rclone -v --config="rclone.conf" copy "$3" "DRIVE:$RCLONE_DESTINATION" 2>&1	
+	rclone -v --config="rclone.conf" copy "$3" "$RCLONE_1ST_REMOTE:$RCLONE_1ST_DESTINATION" 2>&1	
+	rclone -v --config="rclone.conf" copy "$3" "$RCLONE_2ND_REMOTE:$RCLONE_2ND_DESTINATION" 2>&1	
 elif [[ $2 -gt 1 ]]; then # multiple file
-	rclone -v --config="rclone.conf" copy "$topPath" "DRIVE:$RCLONE_DESTINATION/${relativePath%%/*}"
+	SIZE=$(du -B 1 "$topPath" -s | cut --delimiter='	' -f 1)    
+	# 2GB = 2147483648 bytes
+	# 10GB = 10737418240 bytes
+	#if [[ $SIZE -gt 104857600 ]]; then
+	#	echo 'Zipping file....'
+	#	dirname="$topPath"
+	#	echo $dirname
+	#	echo $topPath
+	#	echo $relativePath
+	#	echo $filePath
+	#	cd "$dirname"
+	#	result="${dirname%"${dirname##*[!/]}"}" # extglob-free multi-trailing-/ trim
+	#	result="${result##*/}"
+	#	filename="$result.zip"
+	#	zip -r "$filename" "."
+	#	filename=$(realpath "$filename")
+	#	echo "$filename"
+	#	cd
+	#	rclone -v --config="rclone.conf" copy "$filename" "$RCLONE_1ST_REMOTE:$RCLONE_1ST_DESTINATION" 2>&1	
+	#	rclone -v --config="rclone.conf" copy "$filename" "$RCLONE_2ND_REMOTE:$RCLONE_2ND_DESTINATION" 2>&1
+	#	echo "FINITO"
+	#else
+		rclone -v --config="rclone.conf" copy "$topPath" "$RCLONE_1ST_REMOTE:$RCLONE_1ST_DESTINATION/${relativePath%%/*}"
+		rclone -v --config="rclone.conf" copy "$topPath" "$RCLONE_2ND_REMOTE:$RCLONE_2ND_DESTINATION/${relativePath%%/*}"
+	#fi
 fi
+
 
 echo "$(($(cat numUpload)-1))" > numUpload # Minus 1
